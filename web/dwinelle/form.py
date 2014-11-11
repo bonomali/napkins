5a41270d543a94f79d83f2a71be514404ca40a38
Jason <jxieeducation@berkeley.edu>
2014-11-11 12:45:13 -0800

    t

--- web/dwinelle/form.py
+++ web/dwinelle/form.py
@@ -1,4 +1,5 @@
 from splinter.browser import Browser
+from pyvirtualdisplay import Display 
 import time
 
 class Form:
@@ -24,6 +25,8 @@ class Form:
 		self.finalSubmit = val
 
 def form_fill(user, form):
+	display = Display(visible=0, size=(800, 600))
+	display.start()
 	browser = Browser()
 	browser.visit(form.url)
 	def doClicks(): #partial_htmls being dealt with
@@ -49,8 +52,9 @@ def form_fill(user, form):
 	browser.execute_script('document.getElementsByClassName("' + form.finalSubmit + '")[0].click()')
 	time.sleep(10)
 	browser.quit()
+	display.stop()
 
-Counsyl = Form("https://www.counsyl.com/careers/software-engineer-new-grad-2015/")
+Counsyl = Form("https://www.counsyl.com/careers/software-engineering-intern-2015/")
 Counsyl.addClicks("https://jobs.lever.co/counsyl/")
 Counsyl.addFillField('name',1)
 Counsyl.addFillField('email',2)
@@ -62,21 +66,21 @@ Counsyl.addAttachField('resume',6)
 Counsyl.addAttachField('cards[86665ab6-19bd-4fd0-a466-96f3acc9ccb9][field0]',6)
 Counsyl.setFinalSubmit('template-btn-submit')
 
-Palentir = Form("http://www.palantir.com/careers/OpenPosApply?id=a0m80000000Lkdw")
-Palentir.addFillField('page:OpenPositionTemplate:j_id16:j_id20',1.1) #First Name
-Palentir.addFillField('page:OpenPositionTemplate:j_id16:j_id28',1.2)
-Palentir.addFillField('page:OpenPositionTemplate:j_id16:j_id32',7)
-Palentir.addFillField('page:OpenPositionTemplate:j_id16:j_id36',8)
-Palentir.addFillField('page:OpenPositionTemplate:j_id16:j_id44',9)
-Palentir.addFillField('page:OpenPositionTemplate:j_id16:j_id52',2)
-Palentir.addFillField('page:OpenPositionTemplate:j_id16:j_id56',3)
-Palentir.addFillField('page:OpenPositionTemplate:j_id16:j_id76',4)
-Palentir.addAttachField('page:OpenPositionTemplate:j_id16:j_id84:ipnputFile:file',6)
-Palentir.addDropDownField('page:OpenPositionTemplate:j_id16:j_id40',5)
-Palentir.addDropDownField('page:OpenPositionTemplate:j_id16:j_id66',1)
-Palentir.addDropDownField('page:OpenPositionTemplate:j_id16:j_id71',2)
-Palentir.addDropDownFieldHelper('option',270)
-Palentir.setFinalSubmit('button button-large')
+Palantir = Form("http://www.palantir.com/careers/OpenPosApply?id=a0m80000000Lkdw")
+Palantir.addFillField('page:OpenPositionTemplate:j_id16:j_id20',1.1) #First Name
+Palantir.addFillField('page:OpenPositionTemplate:j_id16:j_id28',1.2)
+Palantir.addFillField('page:OpenPositionTemplate:j_id16:j_id32',7)
+Palantir.addFillField('page:OpenPositionTemplate:j_id16:j_id36',8)
+Palantir.addFillField('page:OpenPositionTemplate:j_id16:j_id44',9)
+Palantir.addFillField('page:OpenPositionTemplate:j_id16:j_id52',2)
+Palantir.addFillField('page:OpenPositionTemplate:j_id16:j_id56',3)
+Palantir.addFillField('page:OpenPositionTemplate:j_id16:j_id76',4)
+Palantir.addAttachField('page:OpenPositionTemplate:j_id16:j_id84:ipnputFile:file',6)
+Palantir.addDropDownField('page:OpenPositionTemplate:j_id16:j_id40',5)
+Palantir.addDropDownField('page:OpenPositionTemplate:j_id16:j_id66',1)
+Palantir.addDropDownField('page:OpenPositionTemplate:j_id16:j_id71',2)
+Palantir.addDropDownFieldHelper('option',270)
+Palantir.setFinalSubmit('button button-large')
 
 Affirm = Form("https://jobs.lever.co/affirm/41093734-0492-4f7e-b5ab-7fe53f2143e7/apply")
 Affirm.addFillField('name',1)
@@ -99,4 +103,4 @@ Box.addAttachField('resume',6)
 Box.addFillField('phone',3)
 Box.setFinalSubmit('template-btn-submit')
 
-form_dict = {'Quora':Quora, 'Box':Box, 'Palentir':Palentir, 'Counsyl':Counsyl, 'Affirm':Affirm}
+form_dict = {'Quora':Quora, 'Box':Box, 'Palantir':Palantir, 'Counsyl':Counsyl, 'Affirm':Affirm}
--- web/dwinelle/management/commands/init_client.py
+++ web/dwinelle/management/commands/init_client.py
@@ -4,6 +4,9 @@ import sys
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+    	past_ips = Client.objects.all()
+    	for past_ip in past_ips:
+    		past_ip.delete()
     	ip = "http://127.0.0.1:8001/"
     	if len(sys.argv) > 1:
     		ip = sys.argv[1]
