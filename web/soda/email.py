700d2f6a4c6635feb0a878bd23f7c4f820365c9d
Jason <jxieeducation@berkeley.edu>
2014-11-13 20:21:26 -0800

    ttttt

--- web/soda/email.py
+++ web/soda/email.py
@@ -7,14 +7,3 @@ from soda.models import *
 def email_send(subject, message, to_email, from_email=settings.QURSE_EMAIL):
 	send_mail(subject, message, from_email,
     to_email, fail_silently=False)
-
-def company_app_data_email(email):
-	companies = Company.objects.all()
-	message = "Hello,\nHere is the data:\n\n"
-	for company in companies:
-		message += company.name
-		message += "\t"
-		message += company.links
-		message += "\n"
-	message += "\n\nBest,\nTroll Face"
-	email_send("Software Engineer Internship App Data", message, [email])
